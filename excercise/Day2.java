4b0e2db54f81745ef8061a013202a72a9977aa61
Alan Perez <alanp9six@gmail.com>
2018-08-30 22:51:01 -0500

    Update Day2.java

--- excercise/Day2.java
+++ excercise/Day2.java
@@ -12,17 +12,24 @@ public class Day2 { //everything we code will be inside a class
 	public static void main(String[] args) throws FileNotFoundException 
 	{
 		//Exercise 1 
-		//Ask the user for a name, look for the name in the table, if it is found
-		//then output the value associated with the name
-		//if it is not found then indicate this by saying "invalid name"
+		//Ask the user for a name, look for the name in the table, if it is found	
+		//then output the value associated with the name	
+		//if it is not found then indicate this by saying "invalid name"	
 		//Additionally loop back and ask for a new name after searching, the program 
 		//does not have to end
 		Scanner fileIn = new Scanner(new File("table.txt"));
+		Scanner sc = new Scanner(System.in); //Scanning in System.in
+		String textLine = sc.nextLine(); //use nextLine ONCE & use variable
 		while (fileIn.hasNext())
 		{
 			String name = fileIn.next();
 			String value = fileIn.next();
-			System.out.println(name + " " +value);
+			if (textLine.equalsIgnoreCase(name)) {
+			 System.out.println(name + " " +value);
+
+			}
+			else
+				System.err.println("Invalid Name!");
 		}
 	}
 }
